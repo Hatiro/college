9e059097af0c73b2058d1b81a4c9dd2d5cc6d2fc
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2019-11-02 19:56:41 +0200

    Computer architecture tasks

---
+++ computer-architecture/vova/Task1/Program.cs
@@ -0,0 +1,179 @@
+﻿//    Copyright(C) 2019  Vova Lantsov
+
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with this program.If not, see https://www.gnu.org/licenses/.
+
+using System;
+using System.IO;
+using System.Text;
+
+namespace Task1
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Console.InputEncoding = Console.OutputEncoding = Encoding.GetEncoding(1251);
+
+            Console.Write("Введите имя файла: ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            var pathToFile = Console.ReadLine();
+
+            Console.ResetColor();
+            Console.Write("Проверка: ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            if (!File.Exists(pathToFile))
+            {
+                Console.Write("Файл не найден");
+                return;
+            }
+
+            Console.WriteLine("OK");
+            Console.ResetColor();
+
+            Console.Write("Введите количество байтов (1-36): ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            if (!int.TryParse(Console.ReadLine(), out var countOfBytes) || countOfBytes < 1)
+            {
+                countOfBytes = 15;
+            }
+            else if (countOfBytes > 36)
+            {
+                countOfBytes = 36;
+            }
+
+            var file = File.OpenRead(pathToFile);
+            Span<byte> buffer = stackalloc byte[countOfBytes > file.Length ? (int) file.Length : countOfBytes];
+            file.Read(buffer);
+            file.Dispose();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Последовательность байт:");
+            Console.ResetColor();
+
+            for (var i = 0; i < buffer.Length; i++)
+            {
+                Console.Write(buffer[i]);
+
+                if (i != buffer.Length - 1)
+                    Console.Write(' ');
+                else Console.WriteLine();
+            }
+
+            var allBits = GetAllBits(buffer);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Бинарная последовательность:");
+            Console.ResetColor();
+            PrintBits(allBits);
+
+            var shiftedBits = RightShiftBits(allBits, 5);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Сдвиг вправо -> на 5 бит:");
+            Console.ResetColor();
+            PrintBits(shiftedBits);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Сжатие:");
+            Console.ResetColor();
+
+            var shiftedPositions = BytesStartingWithOne(shiftedBits, out var countOfBytesStartingFrom0);
+            Span<byte> zippedBits = stackalloc byte[shiftedPositions.Length * 8];
+            var zippedPosition = 0;
+            foreach (ref readonly var position in shiftedPositions)
+            {
+                shiftedBits.Slice(position, 8).CopyTo(zippedBits.Slice(zippedPosition, 8));
+                zippedPosition += 8;
+            }
+
+            PrintBits(zippedBits);
+
+            Console.WriteLine("Количество байт, которые начинаются с нуля: ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(countOfBytesStartingFrom0);
+        }
+
+        private static void WriteBits(byte b, Span<byte> bits)
+        {
+            for (int i = 7, j = 0; i > -1; i--, j++)
+            {
+                bits[j] = (byte) ((b >> i) & 0b00000001);
+            }
+        }
+
+        private static ReadOnlySpan<byte> GetAllBits(ReadOnlySpan<byte> bytes)
+        {
+            Span<byte> bits = new byte[bytes.Length * 8];
+
+            var i = 0;
+            foreach (ref readonly var b in bytes)
+            {
+                WriteBits(b, bits.Slice(i++ * 8, 8));
+            }
+
+            return bits;
+        }
+
+        private static void PrintBits(ReadOnlySpan<byte> bits)
+        {
+            for (var i = 0; i < bits.Length; i += 8)
+            {
+                foreach (ref readonly var bit in bits.Slice(i, 8))
+                {
+                    Console.Write(bit);
+                }
+
+                if (i < bits.Length - 8)
+                    Console.Write(' ');
+                else Console.WriteLine();
+            }
+        }
+
+        private static ReadOnlySpan<byte> RightShiftBits(ReadOnlySpan<byte> bits, int length)
+        {
+            Span<byte> rightShiftedBits = new byte[bits.Length];
+
+            for (var i = 0; i < bits.Length; i++)
+            {
+                rightShiftedBits[(i + length) % rightShiftedBits.Length] = bits[i];
+            }
+
+            return rightShiftedBits;
+        }
+
+        private static ReadOnlySpan<int> BytesStartingWithOne(ReadOnlySpan<byte> bits, out int countOfBytesStartingFromZero)
+        {
+            countOfBytesStartingFromZero = 0;
+            Span<int> positions = new int[bits.Length];
+            var countOfBytesStartingFromOne = 0;
+
+            for (var i = 0; i < bits.Length; i += 8)
+            {
+                ref readonly var b = ref bits[i];
+
+                if (b == 0)
+                {
+                    countOfBytesStartingFromZero++;
+                }
+                else
+                {
+                    positions[countOfBytesStartingFromOne++] = i;
+                }
+            }
+
+            return positions.Slice(0, countOfBytesStartingFromOne);
+        }
+    }
+}
---
+++ computer-architecture/vova/Task2/Program.cs
@@ -0,0 +1,68 @@
+﻿//    Copyright(C) 2019  Vova Lantsov
+
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with this program.If not, see https://www.gnu.org/licenses/.
+
+using System;
+using System.Linq;
+using System.Text;
+
+namespace Task2
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Console.InputEncoding = Console.OutputEncoding = Encoding.GetEncoding(1251);
+            
+            Console.Write("Введите значение байта: ");
+            var b = !long.TryParse(Console.ReadLine(), out var byteValue) || byteValue < 0L
+                ? (byte) 100
+                : byteValue > 255
+                    ? (byte) (byteValue % 256L)
+                    : (byte) byteValue;
+            Console.WriteLine();
+            
+            Console.WriteLine("Введённый байт в");
+            Console.WriteLine("десятичном представлении — {0}", b);
+            var bitStr = Convert.ToString(b, 2).PadLeft(8, '0');
+            var strWithout7n8 = bitStr.Substring(0, 6);
+            var str7n8 = bitStr.Substring(6);
+            Console.Write("двоичном представлении — {0}", strWithout7n8);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(str7n8);
+            Console.ResetColor();
+            Console.WriteLine();
+
+            var rightShifted = new string(strWithout7n8.Select((c, i) => new {c, i})
+                .OrderByDescending(it => it.i == 5).Select(it => it.c).ToArray());
+            var rightShiftedNum = Convert.ToInt32(rightShifted + str7n8, 2);
+            Console.WriteLine("Циклический сдвиг вправо в");
+            Console.WriteLine("десятичном представлении — {0}", rightShiftedNum);
+            Console.Write("двоичном представлении — {0}", rightShifted);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(str7n8);
+            Console.ResetColor();
+            Console.WriteLine();
+
+            var added = rightShiftedNum + 200;
+            var bytes = BitConverter.GetBytes(added);
+            Console.WriteLine("Результат сложения с числом 200 в");
+            Console.WriteLine("десятичном представлении — {0}", added);
+            Console.WriteLine("двоичном представлении — {0}", string.Join(' ',
+                bytes.Where(it => it != 0).Select(it => Convert.ToString(it, 2)
+                    .PadLeft(8, '0'))));
+        }
+    }
+}
