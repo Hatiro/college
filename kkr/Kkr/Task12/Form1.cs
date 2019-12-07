acb48467acccd4d9b1bf03b77ef349a84f8d775e
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2019-12-07 21:05:36 +0200

    kkr - init12 - initial commit

---
+++ kkr/Kkr/Task12/Form1.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace Task12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
